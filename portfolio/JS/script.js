de923ff318729fe295f7e22328da624e466db3b5
yuka1269 <yuka.noguchi628@gmail.com>
2024-09-27 14:34:28 +0900

    portfolio

--- portfolio/JS/script.js
+++ portfolio/JS/script.js
@@ -26,6 +26,7 @@
 
 
 
+
   $(function(){
     $('.mainvisual').slick({
       arrows: false,
@@ -37,32 +38,13 @@
 
 
 
-  $(function() {
-    const pageTop = $("#js-pageTop").hide();
   
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > 100) {
-        pageTop.fadeIn();
-      } else {
-        pageTop.fadeOut();
-      }
-    });
-  
-  
-    pageTop.click(function() {
-      $("body,html").animate({
-        scrollTop: 0
-      }, 500);
-      return false;
-    });
-  });
 
+$(function(){
+  $(".hamburger").click(function (){
+    $(".hamburger").toggleClass("open");
+    $(".header_nav-sp").fadeToggle();
 
-  $(function(){
-    $(".hamburger").click(function (){
-      $(".hamburger").toggleClass("open");
-      $(".header_nav-sp").fadeToggle();
- 
-  });
 });
-  
+});
+
